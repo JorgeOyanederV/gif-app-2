4e4f8be0f4ed2b3219cf2ab2342dafc9b535028c
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2022-04-15 23:02:33 -0400

    Modificaciones ETC

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -5,17 +5,25 @@ const GifGrid = ({ categoria }) => {
 
    // const [images, setImages] = useState([]);
    const { loading, data: images } = useFetchGifs(categoria)
-
-
+   const [counter, setCounter] = useState(0)
+   // const increment = () => {
+   //    setCounter(counter + 1);
+   // }
+   const increment = () => {
+      setCounter(c => c + 1);
+   }
    return (
       <div>
-         <h3>{categoria}</h3>
+         <button onClick={increment}>Incrementar</button>
+         <h3>{categoria} {counter}</h3>
          {
             loading ? 'Loading' :
-            images.map(img => (
-               <GifItem key={img.id} {...img} />
-            ))
+               images.map(img => (
+                  <GifItem key={img.id} {...img} />
+               ))
          }
+
+
       </div>
    );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GiftExpert2 from './GiftApp2';
+import { Padre } from './components/07-tarea-memo/Padre';
+// import GiftExpert2 from './GiftApp2';
 import './index.css';
 
 ReactDOM.render(
-  <GiftExpert2 />,
+  // <GiftExpert2 />
+  <Padre />,
   document.getElementById('root')
 );
